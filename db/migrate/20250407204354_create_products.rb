c1b141a645432e8dff220e5141ee8a34a5b63a75
Amanda Jones <privyet.amanda@gmail.com>
2025-04-07 16:19:29 -0500

    removing not null constraints from migration files - i had added them there by accident earlier; they are now in the schema

--- db/migrate/20250407204354_create_products.rb
+++ db/migrate/20250407204354_create_products.rb
@@ -1,8 +1,8 @@
 class CreateProducts < ActiveRecord::Migration[8.0]
   def change
     create_table :products do |t|
-      t.text :name, null: false
-      t.integer :price, null: false
+      t.text :name
+      t.integer :price
       t.integer :category_id
 
       t.timestamps
--- db/migrate/20250407210535_create_product_categories.rb
+++ db/migrate/20250407210535_create_product_categories.rb
@@ -1,7 +1,7 @@
 class CreateProductCategories < ActiveRecord::Migration[8.0]
   def change
     create_table :product_categories do |t|
-      t.text :name, null: false
+      t.text :name
 
       t.timestamps
     end
