73b0bb09a4be6ab578abd4d53d6cb1e183c9bd30
Amanda Jones <privyet.amanda@gmail.com>
2025-04-07 16:07:25 -0500

    adding not null constraints to model attrs in schema

--- db/migrate/20250407204354_create_products.rb
+++ db/migrate/20250407204354_create_products.rb
@@ -1,8 +1,8 @@
 class CreateProducts < ActiveRecord::Migration[8.0]
   def change
     create_table :products do |t|
-      t.text :name
-      t.integer :price
+      t.text :name, null: false
+      t.integer :price, null: false
       t.integer :category_id
 
       t.timestamps
