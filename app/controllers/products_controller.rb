ab8db7289670671559c5be811dd1301fd881d994
Amanda Jones <privyet.amanda@gmail.com>
2025-04-09 13:59:39 -0500

    adding longform version of product to be used in product#show (_product is for product#index); linking _product to its respective _product_longform

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,14 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: %i[ show ]
   def index
     @products = Product.all
   end
 
   def show
   end
+
+  private
+    def set_product
+      @product = Product.find(params.expect(:id))
+    end
 end
