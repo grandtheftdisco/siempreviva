ead45680ea2e4c3a24db5bf6b6f6b0af75406dc1
Amanda Jones <privyet.amanda@gmail.com>
2025-04-09 14:45:51 -0500

    updating some fixtures; writing 2 basic route tests to pass

--- test/controllers/marketing_controller_test.rb
+++ test/controllers/marketing_controller_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class MarketingControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get gallery" do
+    get gallery_url
+    assert_response :success 
+  end
 end
--- test/controllers/product_controller_test.rb
+++ test/controllers/product_controller_test.rb
@@ -2,12 +2,7 @@ require "test_helper"
 
 class ProductControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get product_index_url
-    assert_response :success
-  end
-
-  test "should get show" do
-    get product_show_url
+    get products_url
     assert_response :success
   end
 end
