06e360a5fa38ecdbae51ffb90445b912e4ff462f
Amanda Jones <privyet.amanda@gmail.com>
2025-04-11 18:06:14 -0500

    removing not-null constraint on cart amount until I can figure out how to pass cart item prices (ie, sum) into this attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_11_200700) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_11_213734) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -23,7 +23,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_11_200700) do
   end
 
   create_table "carts", force: :cascade do |t|
-    t.integer "total_amount", null: false
+    t.integer "total_amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
