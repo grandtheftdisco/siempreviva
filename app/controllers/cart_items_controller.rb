86c960f1a2a44913d07e1e6281cf31da751bfe2f
Amanda Jones <privyet.amanda@gmail.com>
2025-04-11 17:59:03 -0500

    generating cart items model/views/controller, as well as a currentcart module to define the user's cart; defining relationship between products and cart items

---
+++ app/controllers/cart_items_controller.rb
@@ -0,0 +1,47 @@
+class CartItemsController < ApplicationController
+  include CurrentCart
+  before_action :set_cart, only: %i[ create ]
+  before_action :set_cart_item, only: %i[ update destroy ]
+
+  def new
+    @cart_item = CartItem.new
+  end
+
+  def create
+    product = Product.find(params[:product_id])
+    @cart_item = @cart.cart_items.build(product: product)
+    @cart_item.price = product.price
+    @cart_item.product_id = product.id
+    Rails.logger.debug "Cart: #{@cart.inspect}"
+    Rails.logger.debug "Cart Item: #{@cart_item.inspect}"
+    respond_to do |format|
+      if @cart_item.save
+        format.html { redirect_to show_cart_url(@cart.id),
+          notice: "Item added to bag!" }
+        format.json { render :show, status: :created, location: @cart_item }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @cart_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+
+  end
+
+  def destroy
+  end
+
+  private
+    def set_cart_item
+      @cart_item = CartItem.find(params[:id])
+    end
+
+    def cart_item_params 
+      params.require(:cart_item)
+        .permit(
+          :price, :product_id, :cart_id
+        )
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get "cart_items/create"
+  get "cart_items/update"
+  get "cart_items/new"
+  get "cart_items/destroy"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
