3b878e0df3d8a7d71e44c0a75335b71d27a3587c
Amanda Jones <privyet.amanda@gmail.com>
2025-04-18 15:36:18 -0500

    moving business logic from CartItemsController to Cart model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,12 @@
 class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   validates :session_id, presence: true, uniqueness: true
+
+  def add_product(product)
+    cart_item = cart_items.build(product: product)
+    cart_item.price = product.price
+    cart_item.product_id = product.id
+    self.total_amount += cart_item.price
+    cart_item
+  end
 end
