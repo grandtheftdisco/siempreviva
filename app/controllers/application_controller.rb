2dd5352f082248c905327cdb1376f79991cbcafb
Amanda Jones <privyet.amanda@gmail.com>
2025-04-23 16:05:13 -0500

    cutting out current session setting & defining current cart directly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,11 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  before_action :set_cart
-  before_action :set_current_session
+  before_action :set_current_cart
 
   private
   
-  def set_cart
-    @cart = Cart.find_or_create_by(session_id: session.id.to_s)
-  end
-
-  def set_current_session
-    session_id = session[:session_id]
-    cart = Cart.find_or_create_by(session_id: session_id)
-
-    Current.session = CurrentSession.new(
-      id: session_id,
-      cart: cart
-    )
+  def set_current_cart
+    Current.cart = Cart.find_or_create_by(session_id: session.id.to_s) 
   end
 end
