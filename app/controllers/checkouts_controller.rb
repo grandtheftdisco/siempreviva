a4581c8f906df27f102043318fa681fed32b9c0a
Amanda Jones <privyet.amanda@gmail.com>
2025-04-29 17:06:39 -0500

    adding logic to check for existing PaymentIntents associated with the current Cart, to reuse them per Stripe best practices; also checking for expiration of any existing PaymentIntents & updating Cart total amount as needed

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -5,24 +5,9 @@ class CheckoutsController < ApplicationController
   end
   
   def create
-    # 1. Fetch the current cart and calculate the total amount of it
     @cart = Current.cart
-
-    # 2. Create a PaymentIntent
-      # CONSIDERATIONS:
-      # a. DONE - if the amount changes, be sure to update the PI's amount
-      # b. reuse the same PaymentIntent if the checkout process is interrupted and resumed later 
-      # c. DONE - Make sure that this PaymentIntent gets associated with the current Checkout session
+    determine_if_payment_intent_already_exists(@cart)
     
-    @payment_intent = Stripe::PaymentIntent.create({
-      amount: @cart.total_amount,
-      currency: 'usd',
-      automatic_payment_methods: { enabled: true },
-      metadata: { cart_id: @cart.id }      
-    })
-
-    Rails.logger.debug "payment_intent: #{@payment_intent}"
-    # 3. Create a record in your database to track this checkout
     checkout = Checkout.create(
       payment_intent_id: @payment_intent.id,
       cart_id: @cart.id,
@@ -30,11 +15,55 @@ class CheckoutsController < ApplicationController
       stripe_customer_id: 'test_customerid123',
     )
 
-    # 4. Return the client secret to the frontend
-      # a. this will set up payment collection
     render json: {
      clientSecret: @payment_intent.client_secret,
      checkoutId: checkout.id
     }
+  end
+
+  private
+
+  def determine_if_payment_intent_already_exists(cart)
+    @existing_intent = Checkout.find_by(
+      cart_id: cart.id, 
+    )
+
+    @payment_intent = if @existing_intent
+                        validate_intent(@existing_intent.payment_intent_id)
+                      else
+                        create_new_payment_intent
+                      end
+  end
+
+  def validate_intent(existing_intent_id)
+    begin
+      @payment_intent = Stripe::PaymentIntent.retrieve(
+        existing_intent_id
+      )
+
+      # checks for expired PaymentIntents
+      if Time.now - @payment_intent.created > 24.hours
+        create_new_payment_intent
+      end
+
+      unless @payment_intent.amount == @cart.total_amount
+        Stripe::PaymentIntent.update(
+          @payment_intent.id, 
+          amount: @cart.total_amount
+        )
+      end
+
+    rescue Stripe::InvalidRequestError
+      create_new_payment_intent
+    end
+  end
+
+  def create_new_payment_intent
+    @payment_intent = Stripe::PaymentIntent.create({
+      amount: @cart.total_amount,
+      currency: 'usd',
+      automatic_payment_methods: { enabled: true },
+      metadata: { cart_id: @cart.id }      
+    })
   end
 end
