5446203a36f5084fe5da07df7ca146ef6645607d
Amanda Jones <privyet.amanda@gmail.com>
2025-04-30 16:04:03 -0500

    logic adjustment to use stripe-created products from product dashboard when filling cart; this does not include adjusting products#index to include these same products

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -2,12 +2,32 @@ class CheckoutsController < ApplicationController
   
   def new
     @cart = Current.cart
+
+    @session = Stripe::Checkout::Session.create({
+      ui_mode: 'embedded',
+      line_items: [{
+        price:'price_1RJKDAC2evWDh2V2p8E2Uo8W', # replace w actual price_id
+        quantity: 1, # replace w actual quantity of cart_item
+      }],
+      mode: 'payment',
+      return_url: checkout_success_url,
+      expand: ['line_items']
+    })
   end
   
   def create
     @cart = Current.cart
     determine_if_payment_intent_already_exists(@cart)
     
+    
+
+    Rails.logger.info "Checkout Session created: #{@session.id}"
+    Rails.logger.info "Line items: #{@session.line_items.inspect if @session.line_items}"
+
+    if @session.line_items.nil?
+      Rails.logger.warn "No line items returned in the Checkout Session"
+    end
+
     checkout = Checkout.create(
       payment_intent_id: @payment_intent.id,
       cart_id: @cart.id,
@@ -19,6 +39,10 @@ class CheckoutsController < ApplicationController
      clientSecret: @payment_intent.client_secret,
      checkoutId: checkout.id
     }
+  end
+
+  def show # nee `return`
+    @session = Stripe::Checkout::Session.retrieve(params[:id])
   end
 
   private
