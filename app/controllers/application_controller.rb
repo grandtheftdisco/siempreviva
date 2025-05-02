5f7c04699d072d2d1fc0e5f277ae0fb3645fbe59
Amanda Jones <privyet.amanda@gmail.com>
2025-05-02 10:10:11 -0500

    removing cart-setting from ApplicationController to remove redundancy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,4 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  before_action :set_current_cart
-
-  private
-  
-  def set_current_cart
-    Current.cart = Cart.find_or_create_by(session_id: session.id.to_s)
-  end
 end
