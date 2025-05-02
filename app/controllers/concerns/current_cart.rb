6b2233665f279ce15dfc96dd126918d7f8552482
Amanda Jones <privyet.amanda@gmail.com>
2025-05-02 15:23:28 -0500

    cleaning up CurrentCart to return early; isolating error-handling from business logic with ! and a rescue clause.

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -2,14 +2,13 @@ module CurrentCart
   private
     def set_cart
       @cart = Cart.find_by(id: session[:cart_id]) || Cart.find_by(session_id: session.id.to_s)
-      if @cart.nil?
-        @cart = Cart.create(session_id: session.id.to_s)
-        if @cart.persisted?
-          session[:cart_id] = @cart.id
-          Rails.logger.debug "New Cart Created: #{@cart.inspect}"
-        else
-          Rails.logger.debug "Cart creation failed: #{@cart.errors.full_messages}"
-        end
-      end
+      return if @cart
+
+      @cart = Cart.create!(session_id: session.id.to_s)
+      session[:cart_id] = @cart.id
+      Rails.logger.debug "New Cart Created: #{@cart.inspect}"
+      
+      rescue ActiveRecord::InvalidRecord    
+      Rails.logger.debug "Cart creation failed: #{@cart.errors.full_messages}"
     end
 end
