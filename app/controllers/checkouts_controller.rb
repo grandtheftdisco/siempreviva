ebd419b065252495189b1effe7b274dc186bc290
Amanda Jones <privyet.amanda@gmail.com>
2025-05-02 10:01:40 -0500

    deprecating the 'Current' module

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,11 +1,13 @@
 class CheckoutsController < ApplicationController
-  
+  include CurrentCart
+  before_action :set_cart, only: %i[ new create ]
+
   def new
-    @cart = Current.cart
+    Rails.logger.info "$$$ CheckoutsCtrlr used CurrentCart in its #new action"
   end
   
   def create
-    @cart = Current.cart
+    Rails.logger.info "$$$ CheckoutsCtrlr used CurrentCart in its #new action"
     determine_if_payment_intent_already_exists(@cart)
     
     checkout = Checkout.create(
