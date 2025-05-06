655690b5167960ca83c0c76ee1de000325d09caf
Amanda Jones <privyet.amanda@gmail.com>
2025-05-06 14:33:33 -0500

    adding quantity column to cart_items table with not-null constraint and default value of 1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_18_001627) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_06_193030) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_18_001627) do
     t.integer "cart_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity", default: 1, null: false
   end
 
   create_table "carts", force: :cascade do |t|
