331193f2559d657602f423e68581c9700e8d277d
Amanda Jones <privyet.amanda@gmail.com>
2025-05-08 16:58:32 -0500

    reformatting multi-arg #create calls; adding warnings to self to not make changes to tests or else face the consequences; using fixtures for price attrs

--- test/services/cart/cart_calculator_test.rb
+++ test/services/cart/cart_calculator_test.rb
@@ -19,27 +19,30 @@ class CartCalculatorTest < ActiveSupport::TestCase
   # DO NOT TOUCH THIS TEST ^^^^^^^^^^^^^^^^^^
   ############################################################
 
-
-
-
-
-
-
-
+  ############################################################
+  # DO NOT TOUCH THIS TEST
+  ############################################################
   test "given a non-empty cart, return total of the cart" do
     cart = Cart.create!(session_id: "sdfhsdl")
-    cart_item = CartItem.create!(price: 13, cart_id: cart.id, product_id: products(:two).id)
+    cart_item = CartItem.create!(price: products(:two).price, 
+                                 cart_id: cart.id, 
+                                 product_id: products(:two).id)
     total = Cart::CartCalculator.call(cart:)
 
-    assert_equal(13, total)
+    assert_equal(products(:two).price, total)
   end
 
+  ############################################################
+  # DO NOT TOUCH THIS TEST
+  ############################################################
   test "when an item is added to the cart, the cart's total_amount is updated" do
     cart = Cart.create(session_id: "adjfhsdkf")
-    cart_item = CartItem.create!(price: 6, cart_id: cart.id, product_id: products(:one).id)
+    cart_item = CartItem.create!(price: products(:one).price, 
+                                 cart_id: cart.id, 
+                                 product_id: products(:one).id)
     total = Cart::CartCalculator.call(cart:)
 
-    assert_equal(6, total)
+    assert_equal(products(:one).price, total)
   end
 
   # test "when a 2nd instance of a product is added to a cart, the existing cart item's quantity gets updated" do
