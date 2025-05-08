7fea1197e18fcd7c1a4b56452e7e7c67c18be516
Amanda Jones <privyet.amanda@gmail.com>
2025-05-08 17:19:54 -0500

    5 working tests; CartCalculator can update cart total by summing product/cart item prices; placeholder for next test to be written

--- test/services/cart/cart_calculator_test.rb
+++ test/services/cart/cart_calculator_test.rb
@@ -1,7 +1,6 @@
 require "test_helper"
 
 class CartCalculatorTest < ActiveSupport::TestCase
-
   ############################################################
   # DO NOT TOUCH THIS TEST
   ############################################################
@@ -24,19 +23,18 @@ class CartCalculatorTest < ActiveSupport::TestCase
   ############################################################
   test "given a non-empty cart, return total of the cart" do
     cart = Cart.create!(session_id: "sdfhsdl")
-    cart_item = CartItem.create!(price: products(:two).price, 
+    cart_item = CartItem.create!(price: products(:one).price, 
                                  cart_id: cart.id, 
-                                 product_id: products(:two).id)
+                                 product_id: products(:one).id)
     total = Cart::CartCalculator.call(cart:)
-
-    assert_equal(products(:two).price, total)
+    assert_equal(products(:one).price, total)
   end
 
   ############################################################
   # DO NOT TOUCH THIS TEST
   ############################################################
   test "when an item is added to the cart, the cart's total_amount is updated" do
-    cart = Cart.create(session_id: "adjfhsdkf")
+    cart = Cart.create!(session_id: "adjfhsdkf")
     cart_item = CartItem.create!(price: products(:one).price, 
                                  cart_id: cart.id, 
                                  product_id: products(:one).id)
@@ -45,7 +43,40 @@ class CartCalculatorTest < ActiveSupport::TestCase
     assert_equal(products(:one).price, total)
   end
 
-  # test "when a 2nd instance of a product is added to a cart, the existing cart item's quantity gets updated" do
-  
-  # test adding a different product to a cart that already has a cart item
+  ############################################################
+  # DO NOT TOUCH THIS TEST
+  ############################################################
+  test "when a 2nd item is added to a cart, the cart total should match the sum of the 2 cart items'" do
+    cart = Cart.create!(session_id: "adjfhsdkf")
+    cart_item_one = CartItem.create!(price: products(:one).price,
+                                     cart_id: cart.id,
+                                     product_id: products(:one).id)
+    cart_item_two = CartItem.create!(price: products(:two).price,
+                                     cart_id: cart.id,
+                                     product_id: products(:two).id)
+
+    total = Cart::CartCalculator.call(cart:)
+
+    assert_equal(products(:one).price + products(:two).price, total)
+  end
+
+  test "when a 2nd instance of a product is added, the cart total is double the price of the product" do
+    cart = Cart.create!(session_id: "adjfhsdkf")
+    cart_item_first = CartItem.create!(price: products(:one).price,
+                                       cart_id: cart.id,
+                                       product_id: products(:one).id)
+    cart_item_second = CartItem.create!(price: products(:one).price,
+                                        cart_id: cart.id,
+                                        product_id: products(:one).id)
+
+    total = Cart::CartCalculator.call(cart:)
+
+    assert_equal(products(:one).price * 2, total)
+  end
+
+
+  # test "when a 2nd instance of a product is added to a cart, the cart item's quantity should increment by one" do
+    
+  # end
+  # use quantity col as a multiplier for the price when calculating
 end
