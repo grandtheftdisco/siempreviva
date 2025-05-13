1f502fd4431fe1778b2b9707f31f7940fb8fd77f
Amanda Jones <privyet.amanda@gmail.com>
2025-05-13 16:11:45 -0500

    removing my warnings-to-self to not touch extant tests because the message has sunk in at this point

--- test/services/cart/cart_calculator_test.rb
+++ test/services/cart/cart_calculator_test.rb
@@ -1,9 +1,6 @@
 require "test_helper"
 
 class CartCalculatorTest < ActiveSupport::TestCase
-  ############################################################
-  # DO NOT TOUCH THIS TEST
-  ############################################################
   test "given an empty cart, total should be $0" do
     #  step 1: make new cart
     cart = Cart.create!(session_id: "123fdsdsfd")
@@ -14,13 +11,7 @@ class CartCalculatorTest < ActiveSupport::TestCase
     # step 3: assertion - expected first, actual second
     assert_equal(0, total)
   end
-  ############################################################
-  # DO NOT TOUCH THIS TEST ^^^^^^^^^^^^^^^^^^
-  ############################################################
 
-  ############################################################
-  # DO NOT TOUCH THIS TEST
-  ############################################################
   test "when an item is added to the cart, the cart's total_amount is updated" do
     cart = Cart.create!(session_id: "adjfhsdkf")
     cart_item = CartItem.create!(price: products(:one).price, 
@@ -31,9 +22,6 @@ class CartCalculatorTest < ActiveSupport::TestCase
     assert_equal(products(:one).price, total)
   end
 
-  ############################################################
-  # DO NOT TOUCH THIS TEST
-  ############################################################
   test "when a 2nd item is added to a cart, the cart total should match the sum of the 2 cart items" do
     cart = Cart.create!(session_id: "adjfhsdkf")
     cart_item_one = CartItem.create!(price: products(:one).price,
@@ -47,9 +35,6 @@ class CartCalculatorTest < ActiveSupport::TestCase
     assert_equal(products(:one).price + products(:two).price, total)
   end
 
-  ############################################################
-  # DO NOT TOUCH THIS TEST
-  ############################################################
   test "when a 2nd instance of a product is added, the cart total is double the price of the product" do
     cart = Cart.create!(session_id: "adjfhsdkf")
     cart_item_first = CartItem.create!(price: products(:one).price,
