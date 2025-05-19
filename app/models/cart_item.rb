0a0478f70bd64a688436f74aa22a98d433dea318
Amanda Jones <privyet.amanda@gmail.com>
2025-05-19 11:11:15 -0500

    adding model-level validation for default value of cart_item quantity

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,10 +2,16 @@ class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :product
   validates :product_id, uniqueness: { scope: :cart_id }
+  before_validation :set_quantity
   before_validation :set_price
 
   private
 
+  def set_quantity
+    if self.quantity == 0 || !self.quantity
+      self.quantity = 1
+    end
+  end
   def set_price
     self.price ||= product.price if product.present?
   end
