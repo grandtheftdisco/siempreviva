f88f673d4a2a6a2512265728cda501dc61c1e76c
Amanda Jones <privyet.amanda@gmail.com>
2025-05-19 11:36:45 -0500

    writing test for duplicate cart item handling

--- test/services/cart_service/add_to_cart_test.rb
+++ test/services/cart_service/add_to_cart_test.rb
@@ -11,7 +11,13 @@ class AddToCartTest < ActiveSupport::TestCase
     assert_includes(cart.cart_items.reload, new_cart_item)
   end
 
-  # test "when a product has already been added to a cart, adding it again increments the quantity of the existing cart item" do
-    
-  # end
+  test "when a product has already been added to a cart, adding it again increments the quantity of the existing cart item" do
+   cart = carts(:mary)
+   product = products(:lavender)
+   quantity = 1
+   CartService::AddToCart.call(product:, cart:, quantity:)
+   
+   new_cart_item = cart.cart_items.reload.find_by(product_id: product.id)
+   assert_equal(2, new_cart_item.quantity)
+  end
 end
