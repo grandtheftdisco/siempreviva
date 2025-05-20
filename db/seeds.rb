607c61ec39e3a71d1b3e524363327ff9e02882df
Amanda Jones <privyet.amanda@gmail.com>
2025-05-20 13:22:38 -0500

    adding stripe product & price ids to seed products

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,32 +9,44 @@ product_category = ProductCategory.create!(name: "Essential Oils Test Category")
 Product.create!(
   name: 'Melissa', 
   price: 1800, 
-  product_category_id: product_category.id
+  product_category_id: product_category.id,
+  stripe_price_id: 'price_1RKACrC2evWDh2V2MlDwc5z7',
+  stripe_product_id: 'prod_SEdTk1bZbKa6a1'
 )
 Product.create!(
   name: 'Sage',
   price: 900,
-  product_category_id: product_category.id
+  product_category_id: product_category.id,
+  stripe_price_id: 'price_1RKAB0C2evWDh2V2OVPut3p7',
+  stripe_product_id: 'prod_SEdRAILBQj1iOb'
 )
 Product.create!(
   name: 'Immortelle',
   price: 2000,
-  product_category_id: product_category.id
+  product_category_id: product_category.id,
+  stripe_price_id: 'price_1RK5efC2evWDh2V2lkvdqQB2',
+  stripe_product_id: 'prod_SEYmDAzbEBHtC5'
 )
 Product.create!(
   name: 'Roman Chamomile',
   price: 1500,
-  product_category_id: product_category.id
+  product_category_id: product_category.id,
+  stripe_price_id: 'price_1RK5cIC2evWDh2V2kvNl4dFV',
+  stripe_product_id: 'prod_SEYjnKbDeuvVQW'
 )
 Product.create!(
   name: 'Hyssop',
   price: 1100,
-  product_category_id: product_category.id
+  product_category_id: product_category.id,
+  stripe_price_id: 'price_1RK5fSC2evWDh2V21BNqbarM',
+  stripe_product_id: 'prod_SEYinuRcBDegmN'
 )
 Product.create!(
   name: 'Lavender',
   price: 1000,
-  product_category_id: product_category.id
+  product_category_id: product_category.id,
+  stripe_price_id: 'price_1RJKDAC2evWDh2V2p8E2Uo8W',
+  stripe_product_id: 'prod_SDljakyHifZra3'
 )
 puts "#{Product.count} products created"
 
