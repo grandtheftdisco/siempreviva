91fa28128fbc8b314ee1b9475a819aa6a05facc4
Amanda Jones <privyet.amanda@gmail.com>
2025-05-29 15:56:44 -0500

    dropping Product & ProductCategory tables from schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_22_171527) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_29_204332) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -41,22 +41,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_22_171527) do
     t.text "stripe_customer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "product_categories", force: :cascade do |t|
-    t.text "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "products", force: :cascade do |t|
-    t.text "name", null: false
-    t.integer "price", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "product_category_id"
-    t.text "stripe_product_id"
-    t.text "stripe_price_id"
-    t.text "description"
   end
 end
