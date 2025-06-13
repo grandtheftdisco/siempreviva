b4850c273ea6c611c90f482fda52a9c86d6d8f57
Amanda Jones <privyet.amanda@gmail.com>
2025-06-13 16:39:54 -0500

    adding Order model/table; adding test/fixtures from generator; adding tracking number col to table

--- db/migrate/20250612190411_create_orders.rb
+++ db/migrate/20250612190411_create_orders.rb
@@ -2,9 +2,10 @@ class CreateOrders < ActiveRecord::Migration[8.0]
   def change
     create_table :orders do |t|
       t.text :payment_intent_id
-      t.integer :cart_id
-      t.text :status
       t.text :stripe_customer_id
+      t.text :customer_email_address
+      t.integer :amount
+      t.text :status
 
       t.timestamps
     end
