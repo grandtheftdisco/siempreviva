47a05def1b38bfc3c5f7646347e87144f95f9531
Amanda Jones <privyet.amanda@gmail.com>
2025-06-20 13:07:57 -0500

    fixing event var in AdminMailer#event_notification to be an instance var so it can get passed to the view properly; adding AdminMailer#payment_issue_notification to handle unexpected payment intent issues similarly to unexpected events -- with the intent that I/future devs can code a solution if any of these unexpected events begin to happen more often

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,8 +1,15 @@
 class AdminMailer < ApplicationMailer
   def event_notification(event)
-    event = event
-    admin_email = grandtheftdisco@gmail.com
+    @event = event
+    admin_email = 'grandtheftdisco@gmail.com'
 
-    mail to: admin_email, subject: "Unexpected Stripe Event: #{event.object}"
+    mail to: :admin_email, subject: "Unexpected Stripe Event: #{event.object}"
+  end
+
+  def payment_issue_notification(checkout_session, payment_intent)
+    @checkout_session = checkout_session
+    @payment_intent = payment_intent
+
+    mail to: :admin_email, subject: "Unexpected Issue with Payment Intent #{@payment_intent.id}"
   end
 end
