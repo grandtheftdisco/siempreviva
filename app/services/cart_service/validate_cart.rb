b7fcd23166a16c7853e873f600f5df1e71337b29
Amanda Jones <privyet.amanda@gmail.com>
2025-06-25 16:16:09 -0500

    updating args to use keyword argument syntax instead of positional

--- app/services/cart_service/validate_cart.rb
+++ app/services/cart_service/validate_cart.rb
@@ -1,8 +1,8 @@
 module CartService
   class ValidateCart < ApplicationService
     def self.call(cart:)
-      cart, unavailable_items = CheckItemInventory.call(cart)
-      CheckItemPrices.call(cart)
+      cart, unavailable_items = CheckItemInventory.call(cart:)
+      CheckItemPrices.call(cart:)
 
       cart.reload
       [cart, unavailable_items]
