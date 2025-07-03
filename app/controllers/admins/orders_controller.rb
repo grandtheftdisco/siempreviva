0d65f936cec6e0c615c1ed28fdc353a30fa76eb8
Amanda Jones <privyet.amanda@gmail.com>
2025-07-03 15:19:32 -0500

    wiring up order form so that adding a tracking number updates 1) postgres db AND 2) stripe (the tracking number gets added to the related payment intent object's metadata -- next step is to refactor this into a service object

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -1,8 +1,9 @@
 module Admins
   class OrdersController < Admins::ApplicationController
+    before_action :set_order, only: %i[ edit update show ]
+
     def index
-      @all_orders = Order.all
-      @unfulfilled_orders = Order.where(tracking_number = nil)
+      @unfulfilled_orders = Order.where(tracking_number: nil)
     end
 
     def show
@@ -12,6 +13,36 @@ module Admins
     end
 
     def update
+      # needs to move to service object
+      begin
+        intent = Stripe::PaymentIntent.update(
+          @order.payment_intent_id,
+          metadata: { tracking_number: @order.tracking_number }
+        )
+        Rails.logger.info "Successfully added tracking number to PaymentIntent #{intent.id}"
+      rescue Stripe::StripeError => e
+        Rails.logger.warn "Error: #{e.message}"
+      end
+
+
+      if @order.update(order_params)
+        redirect_to admins_orders_url
+        # FIXME - this ain't showin
+        flash.now[:alert] = "Order was successfully updated."
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def set_order
+      @order = Order.find(params[:id])
+    end
+
+    def order_params
+      params.require(:order)
+            .permit(:tracking_number)
     end
   end
 end
