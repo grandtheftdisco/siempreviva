c94a15cb56ab9debe1ebb34c660ee3cae48465e0
Amanda Jones <privyet.amanda@gmail.com>
2025-07-03 21:14:24 -0500

    skipping unneeded operation to assign cart in webhooks ctrlr

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,7 +1,7 @@
 class WebhooksController < ApplicationController
   skip_before_action :verify_authenticity_token
   skip_before_action :require_authentication
-
+  skip_before_action :set_current_cart
   def create
     payload = request.body.read
     sig_header = request.env['HTTP_STRIPE_SIGNATURE']
