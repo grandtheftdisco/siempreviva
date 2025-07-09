cff09a0a2131bae68276ff74cfe7f950b38ac2b6
Amanda <grandtheftdisco@gmail.com>
2025-07-09 16:13:55 -0500

    adding a timestamp formatting helper for order views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def to_dollars(item_price)
     number_to_currency((item_price / 100).round(2))
   end
+
+  def format_time(timestamp)
+    timestamp.strftime('%m/%d/%Y, %I:%M%P (%Z)')
+  end
 end
