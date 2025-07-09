b162c85883b2511f146d74b857e44faceb89ca5b
Amanda <grandtheftdisco@gmail.com>
2025-07-09 16:38:22 -0500

    implementing an update of database Order to mark it as shipped when a tracking number is added to it for future commit that will use this status as a param for #archive view filtering; also -- pulling code out of UpdateOrderTracking.call & placing into private methods

--- app/services/admin_service/update_order_tracking.rb
+++ app/services/admin_service/update_order_tracking.rb
@@ -1,6 +1,13 @@
 module AdminService
   class UpdateOrderTracking < ApplicationService
     def self.call(order:)
+      send_tracking_number_to_stripe(order)
+      update_order_in_database(order)
+    end
+
+    private
+
+    def self.send_tracking_number_to_stripe(order)
       Rails.logger.debug "TRACKING NUMBER FOR ORDER: #{order.tracking_number} <-------"
       begin
         intent = Stripe::PaymentIntent.update(
@@ -13,6 +20,11 @@ module AdminService
       rescue Stripe::StripeError => e
         Rails.logger.warn "Error: #{e.message}"
       end
+    end
+
+    def self.update_order_in_database(order)
+      order.update(status: 'shipped')
+      Rails.logger.debug "\e[3;45mORDER UPDATE: order #{order.id} has a status of '#{order.status}'"
     end
   end
 end
