264c1c2a8e20f74e215eaf7457c91065b64115be
Amanda <grandtheftdisco@gmail.com>
2025-08-06 15:52:22 -0500

    deprecating ProductWrapper in favor of using expandable attributes to fetch Price data for a given product; refactoring Stripe API calls across several classes into a single StripeService::FetchProductInventory service object; removing unused methods to set & format prices of cart items

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,11 +2,6 @@ class CartItem < ApplicationRecord
   belongs_to :cart
   validates :stripe_product_id, uniqueness: { scope: :cart_id }
   before_validation :set_quantity
-  before_validation :set_price
-
-  def price_in_dollars
-    self.price / 100.00
-  end
 
   private
 
@@ -14,14 +9,5 @@ class CartItem < ApplicationRecord
     if self.quantity == 0 || !self.quantity
       self.quantity = 1
     end
-  end
-
-  def set_price
-    @products = Stripe::Product.list(active: true, limit: 100).map do |product|
-      ProductWrapper.new(product)
-    end
-
-    related_product = Stripe::Product.retrieve(self.stripe_product_id)
-    self.price ||= related_product.price if related_product.present?
   end
 end
