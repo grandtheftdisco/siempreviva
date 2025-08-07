c5fe4c1f87adbdd22599d764ba782cf829c779c5
Amanda <grandtheftdisco@gmail.com>
2025-08-07 14:44:15 -0500

    cleaning up #call for this service by creating private methods for each procedure

--- app/services/payment_handling_service/handle_successful_payment.rb
+++ app/services/payment_handling_service/handle_successful_payment.rb
@@ -1,13 +1,15 @@
 module PaymentHandlingService
   class HandleSuccessfulPayment < ApplicationService
-    # TODO - clean up the main call def and create private methods for each procedure
     def self.call(checkout_session:, cart:)
       cart.destroy! if cart.present?
-      Rails.logger.debug "the cart actually got destroyed, right?"
-      Rails.logger.debug "to be sure, here is cart: #{cart.inspect}"
-      Rails.logger.debug "\e[95m---->WE GOT AN ORDER BEING CREATED........\e[0m"
       
-      # add Order record to pg db
+      add_order_to_database(checkout_session)
+      update_checkout_in_database(checkout_session)
+    end
+
+    private
+
+    def self.add_order_to_database(checkout_session:)
       @order = Order.create!(
         payment_intent_id: checkout_session.payment_intent,
         stripe_customer_id: checkout_session.customer,
@@ -15,9 +17,10 @@ module PaymentHandlingService
         amount: checkout_session.amount_total,
         status: checkout_session.status
       )
-      # OrderMailer.received(@order).deliver_later
+      OrderMailer.received(@order).deliver_later
+    end
 
-      # update Checkout record in pg db
+    def self.update_checkout_in_database(checkout_session)
       checkout = Checkout.find_by(stripe_checkout_session_id: checkout_session.id)
       checkout.update(status: "awaiting shipment")
     end
