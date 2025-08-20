0f6b011a25ec76fc78f6859dcaa3fd2fe7c929f2
Amanda <grandtheftdisco@gmail.com>
2025-08-20 15:51:00 -0500

    implementing basic search UI; abstracting algolia index job's perform method into private methods

--- app/jobs/update_algolia_index_job.rb
+++ app/jobs/update_algolia_index_job.rb
@@ -2,32 +2,35 @@ class UpdateAlgoliaIndexJob < ApplicationJob
   queue_as :default
 
   def perform
-    # 1. retrieve products from Stripe using search API
-    products = Stripe::Product.search({query: 'active:"true"'})
+    products = retrieve_products_from_stripe
+    return if products.empty?
 
-    # 2. transform Stripe data to algolia-friendly format
-    algolia_records = products.data.map do |product|
+    algolia_records = convert_data_for_algolia(products)
+
+    push_data_to_algolia(algolia_records)
+  end
+
+  private
+
+  def retrieve_products_from_stripe
+    Stripe::Product.search({query: 'active:"true"'})
+  end
+
+  def convert_data_for_algolia(products)
+    products.data.map do |product|
       {
         objectId: product.id,
         name: product.name,
         description: product.description
       }
     end
+  end
 
-    # 3. push data to algolia
-    # Example usage in your job or service
+  def push_data_to_algolia(algolia_records)
     client = Algolia::SearchClient.create(Rails.application.credentials.algolia[:application_id],
                                           Rails.application.credentials.algolia[:write_api_key])
     client.save_objects("products_index", algolia_records)
-
-    Rails.logger.debug "\e[101;1m----> UpdateAlgoliaIndexJob: #{algolia_records}\e[0"
-
-    algolia_records
     
-    # next steps
-    # 4. schedule this to run periodically
-    # 5. consider implementing webhook listeners for stripe product events to update in real time
-
-    # 6. implement incremental updates: use Stripe's `created` timestamp to only fetch products that have been created or modified since your last sync
+    algolia_records
   end
 end
--- test/jobs/update_algolia_index_job_test.rb
+++ test/jobs/update_algolia_index_job_test.rb
@@ -1,10 +1,11 @@
 require "test_helper"
 
 class UpdateAlgoliaIndexJobTest < ActiveJob::TestCase
+  # FIXME - right now this test is broken because of how I'm trying to restructure the job
   test "when job runs, an array of algolia records will be populated" do
     test_records = UpdateAlgoliaIndexJob.perform_now
 
-    puts test_records.inspect
+    puts test_records
     assert_not_empty(test_records)
   end
 end
