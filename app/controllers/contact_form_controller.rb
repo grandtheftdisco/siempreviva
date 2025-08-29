85a31b95e6c397132fb659c1a498ba0c8da24232
Amanda <grandtheftdisco@gmail.com>
2025-08-29 15:14:02 -0500

    Claude suggestions to adhere to Airbnb style guide; mass revision of controllers; removing notes/debug lines; redoing flash alert partial to handle all flash types

--- app/controllers/contact_form_controller.rb
+++ app/controllers/contact_form_controller.rb
@@ -1,23 +1,32 @@
 class ContactFormController < ApplicationController
+  skip_before_action :require_authentication
+
   def new
     @contact_form = ContactForm.new
   end
 
   def create
-    @contact_form = ContactForm.new(params[:contact_form])
+    @contact_form = ContactForm.new(contact_form_params)
     @contact_form.request = request
 
     respond_to do |format|
       if @contact_form.deliver
-        format.html { redirect_to contact_path,
-                      alert: "Message sent successfully!" }
-        format.json { render json: "Message sent successfully!"}
+        format.html do
+          redirect_to contact_path, notice: 'Message sent successfully!'
+        end
+        format.json { render json: 'Message sent successfully!' }
       else
-        format.html { render :new,
-                      status: :unprocessable_entity }
-        format.json { render json: @contact_form.errors,
-                      status: :unprocessable_entity }
+        format.html { render :new, status: :unprocessable_entity }
+        format.json do
+          render json: @contact_form.errors, status: :unprocessable_entity
+        end
       end
     end
+  end
+
+  private
+
+  def contact_form_params
+    params.require(:contact_form).permit(:name, :email, :message, :nickname)
   end
 end
--- app/models/contact_form.rb
+++ app/models/contact_form.rb
@@ -2,13 +2,13 @@ class ContactForm < MailForm::Base
   attribute :name, validate: true
   attribute :email, validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message
-  # spam protection
-  attribute :nickname, captcha: true 
-  
+  # Spam protection
+  attribute :nickname, captcha: true
+
   def headers
     {
-      subject: "Contact Form Test",
-      to: "siemprevivaoils@gmail.com",
+      subject: 'Contact Form Test',
+      to: 'siemprevivaoils@gmail.com',
       from: %("#{name}" <#{email}>)
     }
   end
