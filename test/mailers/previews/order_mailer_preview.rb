bc06383f7f947b6b8beeea01c98981a6a135b465
Amanda <grandtheftdisco@gmail.com>
2025-11-14 17:19:24 -0600

    refining logic in mailer preview

--- test/mailers/previews/order_mailer_preview.rb
+++ test/mailers/previews/order_mailer_preview.rb
@@ -2,13 +2,13 @@
 class OrderMailerPreview < ActionMailer::Preview
   # Preview this email at http://localhost:3000/rails/mailers/order_mailer/received
   def received
-    order = Order.last || Order.first
+    order = Order.last
     OrderMailer.received(order)
   end
 
   # Preview this email at http://localhost:3000/rails/mailers/order_mailer/shipped
   def shipped
-    order = Order.last || Order.first
+    order = Order.last
     OrderMailer.shipped(order)
   end
 end
