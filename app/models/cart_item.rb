f9833fda7e8e2ee782998610238faac01b6d768a
Amanda <grandtheftdisco@gmail.com>
2026-02-26 09:55:09 -0600

    refining cart item model callback to remove ' quantity == 0 ' case, since this will be handled in the controller's #update action instead

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -8,9 +8,6 @@ class CartItem < ApplicationRecord
   private
 
   def set_quantity
-    # FIXME - refine this logic to handle 0-val quantities differently than when self.quanity has changed to a value other than 0
-    if self.quantity == 0 || !self.quantity
-      self.quantity = 1
-    end
+    self.quantity ||= 1
   end
 end
