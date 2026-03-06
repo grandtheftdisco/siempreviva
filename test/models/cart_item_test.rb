afee056da0a9fa34ef021387998e2b93b1e20ac1
Amanda <grandtheftdisco@gmail.com>
2026-03-06 11:19:59 -0600

    writing tests for CartItem quantity controls

--- test/models/cart_item_test.rb
+++ test/models/cart_item_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class CartItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "updating quantity to zero raises CartItemRemoved" do
+    cart_item = cart_items(:lavender)
+    assert_raises CartItem::CartItemRemoved do
+      cart_item.update!(quantity: 0)
+    end
+    assert_not cart_item.destroyed?, "model signals removal but does not destroy itself"
+  end
+
+  test "updating quantity to a valid value does not destroy the cart item" do
+    cart_item = cart_items(:lavender)
+    cart_item.update!(quantity: 5)
+    assert_not cart_item.destroyed?
+    assert_equal 5, cart_item.reload.quantity
+  end
+
+  test "quantity must be between 1 and 10" do
+    cart_item = cart_items(:lavender)
+    assert_not cart_item.update(quantity: 11)
+    assert_not cart_item.update(quantity: -1)
+  end
 end
